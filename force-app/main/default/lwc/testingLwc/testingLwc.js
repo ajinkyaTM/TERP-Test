1c875cb08140aa2d5ab04d9309c1e28263255fb3
AjinkyaTM <ajinkya_dange@technologymindz.com>
2025-04-18 18:37:13 +0530

    lwc commit

--- force-app/main/default/lwc/testingLwc/testingLwc.js
+++ force-app/main/default/lwc/testingLwc/testingLwc.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class TestingLwc extends LightningElement {}
+export default class TestingLwc extends LightningElement {
+    connectedCallback(){
+        console.log('abcd');
+    }
+}
