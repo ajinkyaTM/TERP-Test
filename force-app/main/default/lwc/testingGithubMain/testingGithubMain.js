713636b77bae0cf91ea6a11dc08b765bb22efff9
Ashusharma-69 <ashusharma15032004@gmail.com>
2025-04-21 17:33:29 +0530

    Changes

--- force-app/main/default/lwc/testingGithubMain/testingGithubMain.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class TestingGithubMain extends LightningElement {
-    connectedCallback(){
-        console.log('deploy to main');
-    }
-}
--- force-app/main/default/lwc/testingLwc/testingLwc.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class TestingLwc extends LightningElement {
-    connectedCallback(){
-        console.log('abcd');
-        console.log('abcdef');
-        console.log('abcdefgh');
-    }
-}
