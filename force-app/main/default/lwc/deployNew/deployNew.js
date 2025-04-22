ff4a67deeef3f53c9e082b9e517d43331e2dbb9f
AjinkyaTM <ajinkya_dange@technologymindz.com>
2025-04-22 21:58:56 +0530

    fg

---
+++ force-app/main/default/lwc/deployNew/deployNew.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class DeployNew extends LightningElement {
+    connectedCallback() {
+        console.log('Deploytesting connectedCallback()');
+    }
+}
